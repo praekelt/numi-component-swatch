b90c81f76693434ebd083641868e388cd5e0c2fc
justinvdm <justinvdm.0.1.0@gmail.com>
2015-08-19 20:20:28 +0200

    Add jscs linting task

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,22 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    jscs: {
+
+      // TODO js paths
+      all: [
+        'gruntfile.js'
+      ]
+
+    }
   });
+
+
+  grunt.loadNpmTasks('grunt-jscs');
 
   grunt.registerTask('default', []);
   grunt.registerTask('build', []);
-  grunt.registerTask('lint', []);
+  grunt.registerTask('lint', ['jscs']);
   grunt.registerTask('test', []);
-  grunt.registerTask('ci', []);
+  grunt.registerTask('ci', ['lint']);
   grunt.registerTask('perf', []);
 };
