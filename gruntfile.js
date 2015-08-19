85e52f937234b7d7587ec788bc9333669caeeda7
justinvdm <justinvdm.0.1.0@gmail.com>
2015-08-19 16:33:30 +0200

    Add initial boilerplate

---
+++ gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+  });
+
+  grunt.registerTask('default', []);
+  grunt.registerTask('build', []);
+  grunt.registerTask('lint', []);
+  grunt.registerTask('test', []);
+  grunt.registerTask('ci', []);
+  grunt.registerTask('perf', []);
+};
