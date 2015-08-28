61032e147d1afd36c8a3139c7a9c3deae1ccf812
justinvdm <justinvdm.0.1.0@gmail.com>
2015-08-28 10:56:20 +0200

    Common enhanced and base stylesheet bundles

--- src/client/views/collection/index.js
+++ src/client/views/collection/index.js
@@ -2,7 +2,7 @@ var loadCSS = require('exports?loadCSS!fg-loadcss');
 var canUseReact = require('../../utils/can-use-react');
 
 if (canUseReact()) {
-  loadCSS('/static/css/collection-enhanced.min.css');
+  loadCSS('/static/css/enhanced.min.css');
 
   require.ensure(['./collection'], function(require) {
     require('./collection')(window._initialState);
